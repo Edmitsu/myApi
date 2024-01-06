7f007f6f3a89f2915e8d7c1f4342c5809e932619
Edmitsu <ed.barcauscas@gmail.com>
2024-01-06 19:10:47 -0300

    Config singleton in RolesRepository

--- src/roles/repositories/RolesRepository.ts
+++ src/roles/repositories/RolesRepository.ts
@@ -5,9 +5,17 @@ type CreateRoleDTO = {
 }
 export class RolesRepository {
   private roles: Role[]
+  private static INSTANCE: RolesRepository
 
-  constructor() {
+  private constructor() {
     this.roles = []
+  }
+
+  public static getInstance(): RolesRepository {
+    if (!RolesRepository.INSTANCE) {
+      RolesRepository.INSTANCE = new RolesRepository()
+    }
+    return RolesRepository.INSTANCE
   }
 
   create({ name }: CreateRoleDTO): Role {
--- src/roles/useCases/createRole/index.ts
+++ src/roles/useCases/createRole/index.ts
@@ -2,6 +2,6 @@ import { RolesRepository } from '@roles/repositories/RolesRepository'
 import { CreateRoleUseCase } from './CreateRoleUseCase'
 import { CreateRoleController } from './CreateRoleController'
 
-const rolesRepository = new RolesRepository()
+const rolesRepository = RolesRepository.getInstance()
 const createRoleUseCase = new CreateRoleUseCase(rolesRepository)
 export const createRolesControler = new CreateRoleController(createRoleUseCase)
